98fcfc2bc827c4542a803c399d05441dff61e856
김도영 <zz01064842675@gmail.com>
2023-01-04 15:57:15 +0900

    Initial commit

---
+++ src/main/java/com/example/withcommuback/WithCommuBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.withcommuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithCommuBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithCommuBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/withcommuback/WithCommuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.withcommuback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithCommuBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
