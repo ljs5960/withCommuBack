cd2e1a505b10507d3a6aefe2bcc466418fd5d747
김도영 <zz01064842675@gmail.com>
2023-01-05 13:56:29 +0900

    db error fix

--- src/main/java/com/example/withcommuback/rep/user/UserRepository.java
+++ src/main/java/com/example/withcommuback/rep/user/UserRepository.java
@@ -1,7 +1,7 @@
-package com.example.withcommuback.rep.user;
-
-import com.example.withcommuback.api.login.vo.LoginVO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<LoginVO, Long> {
-}
+//package com.example.withcommuback.rep.user;
+//
+//import com.example.withcommuback.api.login.vo.LoginVO;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserRepository extends JpaRepository<LoginVO, Long> {
+//}
