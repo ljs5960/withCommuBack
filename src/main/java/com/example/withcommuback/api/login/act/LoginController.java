d6d6a749b8658d7839e2f82a55bd6a75ce3dca9b
김도영 <zz01064842675@gmail.com>
2023-01-06 16:41:03 +0900

    cors fix

--- src/main/java/com/example/withcommuback/api/login/act/LoginController.java
+++ src/main/java/com/example/withcommuback/api/login/act/LoginController.java
@@ -2,18 +2,12 @@ package com.example.withcommuback.api.login.act;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = {"/api"})
+@RequestMapping(value = {"/api/login"})
 public class LoginController {
-    @PostMapping("/signUp/ins")
-    public void test() {
-        log.error("123");
-    }
 }
